9984cde6e1dbd7f7a4e3859515ee04c94b436f43
Nitin Kumar <snitin315@gmail.com>
2019-11-12 20:25:30 +0530

    add edit and destroy doc actions

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -9,11 +9,11 @@ class DocsController < ApplicationController
     end
 
     def new
-        @doc = Doc.new
+        @doc = current_user.docs.build
     end
 
     def create
-        @doc = Doc.new(doc_params)
+        @doc = current_user.docs.build(doc_params)
 
         if @doc.save
             redirect_to @doc
@@ -26,9 +26,16 @@ class DocsController < ApplicationController
     end
 
     def update
+        if @doc.update(doc_params)
+            redirect_to @doc
+        else 
+            render 'edit'
+        end    
     end
 
-    def destroy
+    def destroy 
+        @doc.destroy
+        redirects_to_docs_path
     end
 
     private 
